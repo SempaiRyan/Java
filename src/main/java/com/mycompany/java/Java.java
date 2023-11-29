421a5b2791d206a7de684d5ce90bee8a6c1728d5
ferna <ferna@DESKTOP-A5H7S3N>
2023-11-29 00:05:20 -0500

    a

--- src/main/java/com/mycompany/java/Java.java
+++ src/main/java/com/mycompany/java/Java.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.java;
 
 /**
@@ -13,5 +12,6 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("args = " + args);
+        System.out.println("Hellou");
     }
 }
